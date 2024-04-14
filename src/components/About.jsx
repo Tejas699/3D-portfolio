2e72fc89a283a8608f42e3f38de2b4dbe51c39c9
tejas699 <tjpatil099@gmail.com>
2024-04-14 18:16:39 +0530

    make new project

---
+++ src/components/About.jsx
@@ -0,0 +1,65 @@
+import React from "react";
+import Tilt from "react-tilt";
+import { motion } from "framer-motion";
+
+import { styles } from "../styles";
+import { services } from "../constants";
+import { SectionWrapper } from "../hoc";
+import { fadeIn, textVariant } from "../utils/motion";
+
+const ServiceCard = ({ index, title, icon, info }) => (
+  <Tilt className='xs:w-[325px] w-full'>
+    <motion.div
+      variants={fadeIn("right", "spring", index * 0.5, 0.75)}
+      className='w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card'
+    >
+      <div
+        options={{
+          max: 45,
+          scale: 1,
+          speed: 450,
+        }}
+        className='bg-tertiary rounded-[20px] py-5 px-12 min-h-[310px] flex justify-evenly items-center flex-col'
+      >
+        <div style={{ width: "100%" }} >
+          <img
+            src={icon}
+            alt='web-development'
+            className='w-16 h-16 object-contain rotate linear infinite'
+            // class="rotate linear infinite"
+          />
+        </div>
+
+        <h3 style={{ width: '100%', }} className='text-white text-[20px] font-bold text-left'> {title} </h3>
+        <h6> {info} </h6>
+      </div>
+    </motion.div>
+  </Tilt>
+);
+
+const About = () => {
+  return (
+    <>
+      <motion.div variants={textVariant()}>
+        <p className={styles.sectionSubText}>Introduction</p>
+        <h2 className={styles.sectionHeadText}>Overview.</h2>
+      </motion.div>
+
+      <motion.p
+        variants={fadeIn("", "", 0.1, 1)}
+        className='mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]'
+      >
+        I Know That Good Development
+        Means Good Business
+      </motion.p>
+
+      <div className='mt-20 flex flex-wrap gap-10'>
+        {services.map((service, index) => (
+          <ServiceCard key={service.title} index={index} {...service} />
+        ))}
+      </div>
+    </>
+  );
+};
+
+export default SectionWrapper(About, "about");
