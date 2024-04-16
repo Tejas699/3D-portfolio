5e9d5a6e388aa630c37f9ed8d8182d50e7194971
tejas01999 <tejas.patil@pdrl.in>
2024-04-16 18:21:54 +0530

    adde bg

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,6 +2,7 @@ import { motion } from "framer-motion";
 
 import { styles } from "../styles";
 import { ComputersCanvas } from "./canvas";
+import { Application as gr } from "@splinetool/runtime";
 import Spline from '@splinetool/react-spline';
 
 const Hero = () => {
@@ -26,7 +27,7 @@ const Hero = () => {
         </div>
       </div>
 
-      <ComputersCanvas />
+      {/* <ComputersCanvas /> */}
       <Spline scene="https://prod.spline.design/J7aI6w9pUAysAXjQ/scene.splinecode" />
       {/* <Spline scene="https://prod.spline.design/j8QGEa70QeTCLrsf/scene.splinecode" /> */}
 
